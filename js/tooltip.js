ed71c2df79722a5c7a5d2cf11f63146de3568a39
Paul Smith <paulsmith@pobox.com>
2011-01-24 10:13:57 -0500

    Added blue collar league post

---
+++ js/tooltip.js
@@ -0,0 +1,56 @@
+(function($) {
+  function getPosition(element, tooltip) {
+    var position = $(element).offset(),
+        top = position.top,
+        left = position.left,
+        width = $(tooltip).outerWidth(),
+        height = $(tooltip).outerHeight();
+
+    top -= height;
+    //left += ($(element).outerWidth() / 2) - (width / 2);
+    return {top: top, left: left};
+  }
+
+  function Tooltip(trigger, content) {
+    var self = this,
+        tooltip = $('<div class="tooltip"></div>'),
+        content = $(content, trigger).html();
+
+    tooltip.html(content);
+    $(document.body).append(tooltip);
+
+    $.extend(this, {
+      show: function() {
+        var pos = getPosition(trigger, tooltip);
+
+        tooltip.show();
+        tooltip.css({
+          position: 'absolute',
+          top: pos.top,
+          left: pos.left
+        });
+      },
+
+      hide: function() {
+        tooltip.hide();
+      }
+    });
+
+    $(trigger).bind('mouseenter', function() {
+      self.show();
+    });
+
+    $(trigger).bind('mouseleave', function() {
+      self.hide();
+    });
+  }
+
+  $.fn.tooltip = function(content) {
+    this.each(function() {
+      var tooltip = new Tooltip($(this), content);
+    });
+    return this;
+  };
+})(jQuery);
+
+$('.team td').tooltip('.explanation');
